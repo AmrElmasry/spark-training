9545cf2987e98125d6bd10e323b705020f9356b4
AmrElmasry <amrelmasry.me@gmail.com>
2015-10-23 00:33:52 +0200

    Basic projects completed

--- examples/basics/map/scala/Main.scala
+++ examples/basics/map/scala/Main.scala
@@ -5,6 +5,8 @@ object Main {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-  
+    val words = sc.parallelize(Seq("Sami", "Yomna", "Noha"))
+    words.map(_.toUpperCase).collect
+
   }
 }
--- examples/basics/union/scala/Main.scala
+++ examples/basics/union/scala/Main.scala
@@ -5,6 +5,10 @@ object Main {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-  
+    val group1 = sc.parallelize(Seq(10, 20, 30))
+    val group2 = sc.parallelize(Seq(10, 40, 50))
+
+    group1.union(group2).collect
+
   }
 }
