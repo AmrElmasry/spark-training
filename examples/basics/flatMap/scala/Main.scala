e40697577d69009b62a9c05ca302751a18576bc8
AmrElmasry <amrelmasry.me@gmail.com>
2015-10-23 00:15:00 +0200

    flat map project edited

--- examples/basics/flatMap/scala/Main.scala
+++ examples/basics/flatMap/scala/Main.scala
@@ -5,8 +5,6 @@ object Main {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-    println("55555")
-
     val input = sc.parallelize(Seq("Hello World", "Spark Course"))
     input.flatMap(_.split(" ")).collect
 
