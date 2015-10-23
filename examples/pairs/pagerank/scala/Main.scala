4b886c28c2826c753936f4517e99dbe03ad902d6
AmrElmasry <amrelmasry.me@gmail.com>
2015-10-23 08:54:14 +0200

    pairs projects completed

--- examples/pairs/pagerank/scala/Main.scala
+++ examples/pairs/pagerank/scala/Main.scala
@@ -5,6 +5,22 @@ object Main {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-  
+    val allLines = sc.textFile("../input")
+
+
+    allLines
+      .map(l => {
+        val line = l.split(": ")
+        val secondPart = line(1).split(", ")
+        val length = secondPart.length
+        val weight = 1f / length
+
+        (secondPart, weight)
+
+      }).flatMap(s => s._1.map(_ -> s._2))
+      .reduceByKey(_ + _)
+      .foreach(p => println("The New Value of " + p._1 + " is = " + (.15 +.85 * p._2)))
+
+
   }
 }
--- examples/pairs/wordcount/scala/Main.scala
+++ examples/pairs/wordcount/scala/Main.scala
@@ -5,6 +5,12 @@ object Main {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
+    val input = sc.textFile("../input")
+
+    input.flatMap(_.split(" "))
+      .map(_ -> 1)
+      .reduceByKey(_ + _)
+      .foreach(pair => println("The Word " + pair._1 + " repeated " + pair._2 + " times"))
 
   }
 }
